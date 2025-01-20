8bf38817b31cdbd0a68d4dd0dbcc2597e3b906ef
zoubarevna <melee76@yandex.ru>
2025-01-20 11:03:05 +0300

    Модель данных - Post. [#285211]

---
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -0,0 +1,46 @@
+package ru.job4j.grabber;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Post {
+    private int id;
+    private String title;
+    private String link;
+    private String description;
+    private LocalDateTime created;
+
+    public Post(int id, String title, String link, String description, LocalDateTime created) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post post = (Post) o;
+        return id == post.id && Objects.equals(link, post.link);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, link);
+    }
+
+    @Override
+    public String toString() {
+        return "Post{"
+                + "id=" + id
+                + ", link='" + link + '\''
+                + '}';
+    }
+}
