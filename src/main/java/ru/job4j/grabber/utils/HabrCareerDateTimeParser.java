fb6fb74b27bd786e13e65a0976e97a43ea6ac78f
zoubarevna <melee76@yandex.ru>
2025-01-21 11:11:15 +0300

    2.1. Преобразование даты [#289476]

---
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -0,0 +1,10 @@
+package ru.job4j.grabber.utils;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+public class HabrCareerDateTimeParser implements DateTimeParser {
+    @Override
+    public LocalDateTime parse(String parse) {
+        DateTimeFormatter dtf = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+        return LocalDateTime.parse(parse, dtf);
+    }
+}
---
+++ src/test/java/ru/job4j/grabber/utils/HabrCareerDateTimeParserTest.java
@@ -0,0 +1,14 @@
+package ru.job4j.grabber.utils;
+import org.junit.jupiter.api.Test;
+import java.time.LocalDateTime;
+import static org.assertj.core.api.Assertions.*;
+class HabrCareerDateTimeParserTest {
+    @Test
+    public void simpleTest() {
+        HabrCareerDateTimeParser parser = new HabrCareerDateTimeParser();
+        String input = "2023-09-25T23:58:15+03:00";
+        LocalDateTime expected = LocalDateTime.of(2023, 9, 25, 23, 58, 15);
+        LocalDateTime rsl = parser.parse(input);
+        assertThat(expected).isEqualTo(rsl);
+    }
+}
